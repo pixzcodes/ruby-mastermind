2385895d5767ea9c56795bfe3771e4cc66b17f4a
Pixz <tinycactusking@gmail.com>
2022-10-24 19:12:33 -0400

    Add main loop in Game.play and guess validation

--- mastermind.rb
+++ mastermind.rb
@@ -4,10 +4,24 @@ class Game
     @code_maker = code_maker.new
     @code_maker.create_code
   end
-  def play
-    
-  end
 
+  def play
+    @turns = 0
+    while @turns < 12
+      puts "Turn ##{@turns}"
+      puts "Code Breaker guess:"
+      loop do # get guess and run checks to make sure its valid
+        @code_breaker.get_guess
+        if @code_breaker.guess.length == 4
+          if @code_breaker.guess.all? {|letter| ['r', 'p', 'b', 'y'].any? {|x| x == letter}}
+            break 
+          end
+        end
+        puts "Please enter a valid guess"
+      end
+      @turns += 1
+    end
+  end
 end
 
 class Computer
@@ -21,7 +35,10 @@ class Computer
 end
 
 class Player
-
+  attr_reader :guess
+  def get_guess
+    @guess = gets.chomp.gsub(' ', '').split('')
+  end
 end
 
 Game.new(Player, Computer).play
